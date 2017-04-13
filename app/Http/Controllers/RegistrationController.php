2148fe8b2cd01224100168a61bc346189ec68062
Reymark Torres <rtorres@i4asiacorp.com>
2017-04-13 16:50:03 +0800

    Send user an email after he successfully signed up using Laravel mail facade

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Mail\Welcome;
 use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
@@ -27,6 +28,8 @@ class RegistrationController extends Controller
     	);
 
     	auth()->login($user);
+
+        \Mail::to($user)->send(new Welcome($user));
 
     	return redirect()->home();
     }
